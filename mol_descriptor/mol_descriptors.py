e8d5d0f5867b1a1b8559e02798bcb340bfc2553e
Calvin Yiu <chunnyyiu@hotmail.com>
2022-01-20 16:05:27 +0000

    woop

--- mol_descriptor/mol_descriptors.py
+++ mol_descriptor/mol_descriptors.py
@@ -14,14 +14,15 @@ class mol_descriptor(object):
         self.molecular_rep = None
 
     def generate_basic_atom_props(self):
+        if self.mol.pybmol is None:
+            self.mol.to_pybel()
         self.atom_properties = atom_prop.get_pyb_prop(self.mol.pybmol)
+        if self.mol.rdmol is None:
+            self.mol.to_rdkit()
         self.atom_properties.update(atom_prop.get_rdkit_prop(self.mol.rdmol))
 
     def generate_basic_pair_props(self):
         self.pair_properties = pair_prop.get_pyb_prop(self.mol.pybmol)
-
-    def one_hot_encode(self, atom_prop_key):
-        return one_hot_df
 
     def generate_graph_from_df(atoms_df, pairs_df, graph_type='FC'):
         if graph_type == 'FC':
